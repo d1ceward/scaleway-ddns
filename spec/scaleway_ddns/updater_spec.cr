757fa12b3ad20f72bd10b3e72b89c69513d63a4a
Fabien LEFEBVRE (d1ceward) <contact@d1ceward.com>
2025-08-07 15:13:34 +0200

    Enhance ScalewayDDNS to support both A and AAAA records in address_record_list and update_address_record methods

--- spec/scaleway_ddns/updater_spec.cr
+++ spec/scaleway_ddns/updater_spec.cr
@@ -1,3 +1,9 @@
 require "../spec_helper"
 
-describe ScalewayDDNS::Updater do; end
+describe ScalewayDDNS::Updater do
+  it "can be initialized with a config" do
+    config = ScalewayDDNS::Config.new
+    updater = ScalewayDDNS::Updater.new(config)
+    updater.should be_a(ScalewayDDNS::Updater)
+  end
+end
--- src/scaleway_ddns/request.cr
+++ src/scaleway_ddns/request.cr
@@ -1,48 +1,34 @@
 module ScalewayDDNS
-  # The Request class handles interactions with the Scaleway API for managing DNS records.
   class Request
-    # The host of the Scaleway API used for DNS record management.
     SCW_API_HOST = "api.scaleway.com"
 
-    # Creates a new instance of `ScalewayDDNS::Request` with given Scaleway secret key.
     def initialize(@scw_secret_key : String); end
 
-    # Get a list of A (address) record from the Scaleway API for a given domain.
-    #
-    # ```
-    # ScalewayDDNS::Request.address_record_list("example.com")
-    # # => [{ :id => 1, :name => "" :ttl => 60 }, { :id => 2, :name => "mail" :ttl => 120 }]
-    #
-    # ScalewayDDNS::Request.address_record_list("invalid.com")
-    # # => Scaleway API: Unauthorized, please check configuration variables.
-    # ```
+    # Get a list of A and AAAA records from the Scaleway API for a given domain.
     def address_record_list(domain : String) : Array(Hash(Symbol, String | Int32))
-      Log.info { "Scaleway API: Getting A record for #{domain}" }
+      Log.info { "Scaleway API: Getting address records for #{domain}" }
 
-      # Execute the API request and parse the response
-      response = execute_request("GET", "/domain/v2beta1/dns-zones/#{domain}/records?type=A")
-      records = parse_response(response)["records"]?.try(&.as_a?) || [] of Hash(String, JSON::Any)
-
-      # Transform the parsed records into a more structured format
-      records.map do |record|
-        {
-          :id => record["id"]?.to_s,
-          :data => record["data"]?.to_s,
-          :name => record["name"]?.to_s,
-          :ttl => record["ttl"]?.try(&.as_i) || 60
-        }
+      records = [] of Hash(Symbol, String | Int32)
+      %w(A AAAA).each do |type|
+        response = execute_request("GET", "/domain/v2beta1/dns-zones/#{domain}/records?type=#{type}")
+        recs = parse_response(response)["records"]?.try(&.as_a?) || [] of Hash(String, JSON::Any)
+        records.concat(recs.map do |record|
+          {
+            :id => record["id"]?.to_s,
+            :data => record["data"]?.to_s,
+            :name => record["name"]?.to_s,
+            :ttl => record["ttl"]?.try(&.as_i) || 60,
+            :type => type
+          }
+        end)
       end
+      records
     end
 
-    # Update an A (address) record in the Scaleway API for a given domain.
-    #
-    # ```
-    # ScalewayDDNS::Request.update_address_record("example.com", "127.0.0.1", record)
-    # ```
-    def update_address_record(domain : String, ip : String, record : Hash(Symbol, String | Int32))
-      Log.info { "Scaleway API: Updating A record for #{domain}" }
+    # Update an address record (A or AAAA) in the Scaleway API for a given domain.
+    def update_address_record(domain : String, ip : String, record : Hash(Symbol, String | Int32), record_type : String)
+      Log.info { "Scaleway API: Updating #{record_type} record for #{domain}" }
 
-      # Construct the request body
       body_string = {
         "changes": [
           {
@@ -53,7 +39,7 @@ module ScalewayDDNS
                   "data": "#{ip}",
                   "name": "#{record[:name]}",
                   "ttl": "#{record[:ttl]}",
-                  "type": "A"
+                  "type": record_type
                 }
               ]
             }
@@ -61,7 +47,6 @@ module ScalewayDDNS
         ]
       }.to_json
 
-      # Execute the update request and parse the response.
       parse_response(execute_request("PATCH", "/domain/v2beta1/dns-zones/#{domain}/records", body_string))
     end
 
@@ -70,22 +55,16 @@ module ScalewayDDNS
       endpoint : String,
       body : String? = nil
     ) : HTTP::Client::Response
-      # Set headers and create an HTTP client with a timeout for the connection.
       headers = HTTP::Headers{"X-Auth-Token" => @scw_secret_key}
       client = HTTP::Client.new(URI.new("https", SCW_API_HOST))
       client.connect_timeout = 10.seconds
-
-      # Execute the API request and return the response
       client.exec(method, endpoint, headers, body)
     rescue IO::TimeoutError | Socket::Addrinfo::Error | Socket::ConnectError
       HTTP::Client::Response.new(408)
     end
 
     private def parse_response(response : HTTP::Client::Response) : JSON::Any
-      # If the response status code is 200, parse and return the response body as JSON
       return JSON.parse(response.body) if response.status_code == 200
-
-      # Raise a RequestError with the response status code
       raise RequestError.new(response.status_code)
     end
   end
